94e4bcfcd8b79fd80d2c1383b0cd2d70d97406c0
Rohit Garg <rohitgarg@Rohits-Mac-mini.local>
2021-03-07 11:55:52 +0530

    Working app

---
+++ hello-ios/hello-iosTests/hello_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  hello_iosTests.swift
+//  hello-iosTests
+//
+//  Created by Rohit Garg on 07/03/21.
+//  Copyright © 2021 Rohit Garg. All rights reserved.
+//
+
+import XCTest
+@testable import hello_ios
+
+class hello_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
